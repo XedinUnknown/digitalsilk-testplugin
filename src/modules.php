82487ab5f5930fadbf97b612ec173e964792feb3
Anton Ukhanev <xedin.unknown@gmail.com>
2023-08-30 15:34:39 +0200

    Enable the DummyJSON module

--- src/modules.php
+++ src/modules.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use DigitalSilk\DummyJson\DummyJsonModule;
+
 return function (string $rootDir, string $mainFile): iterable {
     $modules = [
+        new DummyJsonModule(),
     ];
 
     return $modules;
