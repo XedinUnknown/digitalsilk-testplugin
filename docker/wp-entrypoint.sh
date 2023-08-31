3f1ae4c374ca69b56e50f828d7da348445e8bef0
Anton Ukhanev <xedin.unknown@gmail.com>
2023-08-31 18:50:27 +0200

    Add WP Crontrol plugin for debugging
    https://wordpress.org/plugins/wp-crontrol/

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -14,6 +14,7 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
   wp plugin install woocommerce --version="${WC_VERSION}" --activate --allow-root --path="${DOCROOT_PATH}"
+  wp plugin install wp-crontrol --activate --allow-root --path="${DOCROOT_PATH}"
   wp plugin activate "${PLUGIN_NAME}" --allow-root --path="${DOCROOT_PATH}"
 
   # Custom setup instructions
