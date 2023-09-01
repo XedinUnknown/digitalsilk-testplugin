964b9710124285cb3697055a7124a20b216386ae
Anton Ukhanev <xedin.unknown@gmail.com>
2023-09-01 13:08:24 +0200

    Fix uploads directory not being writable
    This prevented product images from being added to media library.
    But it also prevented logging, so it was hard to debug.
    It was due to the `wp-content` directory being owned by `root`, because files were being mounted into `wp-content/plugins` before the server had the chance to set up WP.
    Could not `chown` the whole `wp-content` directory because the mounted files cause FS recursion.

--- docker/wp-entrypoint.sh
+++ docker/wp-entrypoint.sh
@@ -11,6 +11,10 @@ if wait-for-it.sh "${WORDPRESS_DB_HOST}" -t 60; then
     --url="${WP_DOMAIN}" \
     --admin_email="${ADMIN_EMAIL}" \
     --skip-email
+
+  # https://github.com/docker-library/wordpress/issues/436
+  chown -R www-data:www-data "${DOCROOT_PATH}/wp-content/uploads"
+
   wp plugin is-installed akismet --allow-root && wp plugin uninstall akismet --allow-root --path="${DOCROOT_PATH}"
   wp plugin is-installed hello --allow-root && wp plugin uninstall hello --allow-root --path="${DOCROOT_PATH}"
   wp plugin install woocommerce --version="${WC_VERSION}" --activate --allow-root --path="${DOCROOT_PATH}"
